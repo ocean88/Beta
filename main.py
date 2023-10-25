2c6b6f3dc9658fd48150436011c1218bd6e1a35b
Makarov Anatoly <njkzid@yandex.ru>
2023-10-25 22:25:40 +0900

    first pr11

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from src.masks import mask_card_number
-from src.masks import bank_account
+from src.widget import mask_card_number
+from src.widget import bank_account
+from src.widget import convert_date_format
 
 """импорт функции из файла src/masks.py"""
 
-print(mask_card_number(7000792289606361))
+print(mask_card_number("Visa classic", 7000792289606361))
 """передача данных и принт функции"""
 
-print(bank_account(73654108430135874305))
+print(bank_account("Счет",73654108430135874305))
 """передача данных и принт функции"""
 
-"""тест гита"""
+print(convert_date_format("2018-07-11T02:26:18.671407"))
+"""передача данных и принт функции"""
---
+++ src/widget.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+def mask_card_number(bank_name: str, card_number: str) -> str:
+    """Функция Получает номер карты и на выходе приобразует в зашифрованном виде"""
+    card_number = str(card_number)
+    """Числовой тип не подходит для шифрования, поэтому переводим в str"""
+    masked_card_number = (
+        card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[12:]
+    )
+    """через слайсы указываем какие цифры отправить на выход добавляем пустой пробел, затем еще слайсы и так далее"""
+    return f"{bank_name} {masked_card_number}"
+
+
+def bank_account(bank_book: str, bank_account: str) -> str:
+    bank_account = str(bank_account)
+    """Функция Получает номер cчета и на выходе приобразует в зашифрованном виде """
+    masked_bank_account = (
+        "**" + bank_account[16:]
+    )
+    """через слайсы указываем какие цифры отправить на выход добавляем пустой пробел, затем еще слайсы и так далее"""
+    return f"{bank_book} {masked_bank_account}"
+
+
+def convert_date_format(date_string: str) -> str:
+    """Возвращаем в нужном виде через слайсы"""
+
+    return f"{date_string[8:10]}.{date_string[5:7]}.{date_string[0:4]}"
