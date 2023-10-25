8112bc6e6101dc3e0049152bc8f72cd99528bafb
Makarov Anatoly <njkzid@yandex.ru>
2023-10-25 19:12:00 +0900

    testmain

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.masks import mask_card_number
+from src.masks import bank_account
+
+"""импорт функции из файла src/masks.py"""
+
+print(mask_card_number(7000792289606361))
+"""передача данных и принт функции"""
+
+print(bank_account(73654108430135874305))
+"""передача данных и принт функции"""
