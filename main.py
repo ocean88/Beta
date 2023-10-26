a286927d7b955fecab9f9b6aa7a3cca86981e6e3
Makarov Anatoly <njkzid@yandex.ru>
2023-10-26 19:07:11 +0900

    4 commit

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from src.widget import mask_card_number
-from src.widget import bank_account
 from src.widget import convert_date_format
 
 """импорт функции из файла src/masks.py"""
 
-print(mask_card_number("Visa classic", 7000792289606361))
-"""передача данных и принт функции"""
-
-print(bank_account("Счет", 73654108430135874305))
+print(mask_card_number(73654108430135874305))
 """передача данных и принт функции"""
 
 print(convert_date_format("2018-07-11T02:26:18.671407"))
