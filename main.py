c840cb61cd05858c893e3d57b9a45793e887b155
Makarov Anatoly <njkzid@yandex.ru>
2023-10-30 17:15:27 +0900

    Final Version

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from src.widget import mask_card_number
+from src.widget import split_value
 from src.widget import convert_date_format
+"""импорт функции из файла src/widget.py"""
 
-"""импорт функции из файла src.widget"""
 
-print(mask_card_number("Счет", 64686473678894779589))
-"""передача данных и принт функции"""
+print(split_value("MasterCard 7158300734726758"))
 
-print(convert_date_format("2018-07-11T02:26:18.671407"))
-"""передача данных и принт функции"""
+# print(convert_date_format("2018-07-11T02:26:18.671407"))
+# """передача данных и принт функции"""
--- src/widget.py
+++ src/widget.py
@@ -1,12 +1,21 @@
 from src.masks import mask_card_number
+from src.masks import bank_account
+"""импорт функций из masks"""
 
-print(mask_card_number("Счет", 64686473678894779589))
-"""передача данных и принт функции"""
+def split_value(value):
+    """Полученное значения из вызова разделяем на 2 переменные
+    и возвращаем значение в функцию masks"""
+    result = value.split()
+    first_word = result[0]
+    second_word = result[1]
+    """Условие проверки длины и возврат в нужном формате"""
+    if len(second_word) >= 20:
+        return f"{first_word} {bank_account(second_word)}"
+    else:
+        return f"{first_word} {mask_card_number(second_word)}"
+
 
 def convert_date_format(date_string: str) -> str:
     """Возвращаем в нужном виде через слайсы"""
 
     return f"{date_string[8:10]}.{date_string[5:7]}.{date_string[0:4]}"
-
-print(convert_date_format("2018-07-11T02:26:18.671407"))
-"""передача данных и принт функции"""
