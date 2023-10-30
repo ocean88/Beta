9852f62bfda6ef5fc067581717059dedcc4d4d6c
Makarov Anatoly <njkzid@yandex.ru>
2023-10-31 01:37:04 +0900

    Final Version

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.widget import convert_date_format
 """импорт функции из файла src/widget.py"""
 
 
-print(split_value("MasterCard 7158300734726758"))
+print(split_value("Visa Platinum 7158300734726758"))
 
 # print(convert_date_format("2018-07-11T02:26:18.671407"))
 # """передача данных и принт функции"""
