675e243c01e85eeccf4445eac23223be3fdb5eb2
ocean88 <oceann3@gmail.com>
2023-11-19 22:00:04 +0900

    Add utils.py test_utils.py

--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,12 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.info("Запуск модуля masks.py....")
+
+
 def mask_card_number(card_number: str) -> str:
+    logger.info("Запуск функции шифрования карт....")
     """Функция Получает номер карты и на выходе приобразует в зашифрованном виде"""
     card_number = str(card_number)
     """Числовой тип не подходит для шифрования, поэтому переводим в str"""
@@ -6,12 +14,15 @@ def mask_card_number(card_number: str) -> str:
         card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[12:]
     )
     """через слайсы указываем какие цифры отправить на выход добавляем пустой пробел, затем еще слайсы и так далее"""
+    logger.info(f"Входящий аргумент зашифрован в {masked_card_number}")
     return masked_card_number
 
 
 def bank_account(bank_account: str) -> str:
+    logger.info("Запуск функции шифрования счета....")
     bank_account = str(bank_account)
     """Функция Получает номер cчета и на выходе приобразует в зашифрованном виде """
     masked_bank_account = "**" + bank_account[16:]
     """через слайсы указываем какие цифры отправить на выход добавляем пустой пробел, затем еще слайсы и так далее"""
+    logger.info(f"Входящий аргумент зашифрован в {masked_bank_account}")
     return masked_bank_account
